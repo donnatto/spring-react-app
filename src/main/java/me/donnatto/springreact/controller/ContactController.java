e759837bf48e986721d888c4a491d4cc71d25ef9
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-26 13:56:25 -0500

    implement the view

---
+++ src/main/java/me/donnatto/springreact/controller/ContactController.java
@@ -0,0 +1,13 @@
+package me.donnatto.springreact.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ContactController {
+
+  
+}
